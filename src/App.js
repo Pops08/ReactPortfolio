ce0f5d5f1a0ddca15350180fe97e48c6fe743cd3
Joshua Hartshorn <Pops@Joshuas-MacBook-Air-2.local>
2020-07-26 18:40:24 -0700

    First initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import AboutMe from './components/About'
+import Nav from './components/Nav'
+import ContactForm from './components/Contact'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import Portfolio from './components/Portfolio'
 
 function App() {
+
+  const [contactSelected, setContactSelected] = useState(false);
+  const [abSelected, setABSelected] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+<Nav
+  // tabs={tabs}
+  // setCurrentTab={setCurrentTab}
+  // currentTab={currentTab}
+  // contactSelected={contactSelected}
+  // setContactSelected={setContactSelected}
+></Nav>
+      <main>
+        if (condition) {
+          
+        }
+        <AboutMe />
+
+      </main>
     </div>
+
   );
 }
 
