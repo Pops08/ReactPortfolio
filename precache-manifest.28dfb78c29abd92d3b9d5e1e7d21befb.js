7e1e3f6d66a71b013a770232c2f4945eb387b1ca
Joshua Hartshorn <Pops@Joshuas-MacBook-Air-2.local>
2020-07-26 23:20:04 -0700

    Updates

---
+++ precache-manifest.28dfb78c29abd92d3b9d5e1e7d21befb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9521217915f5eb340b485e875e41a29d",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "1b26d6bce352e2715dcf",
+    "url": "/ReactPortfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1b26d6bce352e2715dcf",
+    "url": "/ReactPortfolio/static/js/2.109b5b6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactPortfolio/static/js/2.109b5b6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a237d8719d61ae992db6",
+    "url": "/ReactPortfolio/static/js/main.cc337e4d.chunk.js"
+  },
+  {
+    "revision": "245e88873ef0a2539a5e",
+    "url": "/ReactPortfolio/static/js/runtime-main.ebaa2495.js"
+  }
+]);
